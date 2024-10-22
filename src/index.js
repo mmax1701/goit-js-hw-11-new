d9cc47dc2a9d90a7ba0d8ca5715648715a44feed
mmax1701 <122221954+mmax1701@users.noreply.github.com>
2024-10-22 12:05:03 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,14 +2,24 @@ import axios from "axios";
 import Notiflix from "notiflix";
 
 const KEY = '37350286-5f3aac9a725d44d4223b6e61c'
+const URL = 'https://pixabay.com/api/?key=`${KEY}`&q=dog&image_type=photo&orientation=horizontal&safesearch=true'
 
 const form = document.querySelector('.search-form');
+const input = document.querySelector('input[name="searchQuery"]')
 const button = document.querySelector('button[type="submit"]')
 const list = document.querySelector('.gallery')
 
 form.addEventListener('submit', onSubmit)
-// button.addEventListener('click', onClick)
 
+function onSubmit(evt) {
+    evt.preventDefault();
+    const search = input.value;
+    return search;
+}
+
+function searchImg(search) {
+
+}
 
 
 
